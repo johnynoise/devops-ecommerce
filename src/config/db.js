bb0ea3c2d870acb6fa8a7eb4931903d362f4b178
Johny Noise <79932410+johnynoise@users.noreply.github.com>
2025-04-28 08:08:00 -0400

    aplicação em node

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+  user: 'postgres',
+  password: 'password',
+  host: 'database-1.cfsausgm8chc.us-east-2.rds.amazonaws.com',
+  port: 5432,
+  database: 'launchstore',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
